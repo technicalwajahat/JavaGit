0d16efc99c62d7c5d64fc0c764ec9340dc1502a5
Muhammad Wajahat Asif <wajahat.awan.924@gmail.com>
2021-06-01 16:40:05 +0500

    Add new multiply.java

--- Diff.java
+++ Diff.java
@@ -1,10 +1,10 @@
-public class sum 
+public class diff 
 {
     public static void main(String[] args) 
     {
         int a = 10;
         int b = 5;
         
-        System.out.println("Diff is: "a-b);
+        System.out.println("Diff is: " + a-b);
     }
 }
---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class mul 
+{
+    public static void main(String[] args) 
+    {
+        int a = 15;
+        int b = 9;
+        
+        System.out.println("Product is: " + a*b);
+    }
+}
